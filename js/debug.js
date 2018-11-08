854bac75e1d5a2132419729dc7efe6538e591cae
jean-yves <jean-yves.lecabellec@leetchi.com>
2018-11-08 15:25:47 +0100

    Debug Arrow Function Expressions
    Debug Arrow Function Expressions

--- js/debug.js
+++ js/debug.js
@@ -1,13 +1,11 @@
-const multiplyUs = num1, num2 => console.log(num1 * num2); 
+const multiplyUs = (num1, num2) => console.log(num1 * num2); 
 
-const subtractFromTen = num => 
+const subtractFromTen = num => {
 	const diff = 10 - num; 
 	console.log(diff);
-
-
-const printDate = date = function() {
-	console.log(date);
 }
+
+const printDate = date => console.log(date);
 
 multiplyUs(2,6);
 subtractFromTen(4);
