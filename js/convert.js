f0c386dbacd6df9e03268f64334b107bbd9e1c67
jean-yves <jean-yves.lecabellec@leetchi.com>
2018-11-08 15:21:33 +0100

    Convert to Arrow Function Expressions
    Convert to Arrow Function Expressions

--- js/convert.js
+++ js/convert.js
@@ -1,12 +1,25 @@
-function addToTen(num) {
-  return 10 + num;
-}
+// function addToTen(num) {
+//   return 10 + num;
+// }
 
-function divideUs(num1, num2) {
-  return num1/num2;
-}
+addToTen = num => 10 + num;
+console.log(addToTen(10));
 
-function printMyName() {
+// function divideUs(num1, num2) {
+//   return num1/num2;
+// }
+
+divideUs = (num1, num2) => num1/num2;
+console.log(divideUs(6,2));
+
+// function printMyName() {
+//   let myName = 'Ashley';
+// 	console.log(myName);
+// }
+
+printMyName = () => {
   let myName = 'Ashley';
 	console.log(myName);
 }
+
+console.log(printMyName());
