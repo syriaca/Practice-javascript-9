367258a72141355998769ef8ff833d7d204a2ab2
jean-yves <jean-yves.lecabellec@leetchi.com>
2018-11-08 15:32:24 +0100

    Initial commit

--- js/refactor.js
+++
@@ -1,9 +0,0 @@
-const multiplyByTen = num => 10 * num;
-
-const printToTen = () => {
-	for (let i = 1; i <= 10; i++) {
-		console.log(i);
-	}
-}
-
-const addUs = (num1, num2) => console.log(num1 + num2);
