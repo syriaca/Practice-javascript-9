7e541019c36cc9127b1d16565221db358d950a3e
jean-yves <jean-yves.lecabellec@leetchi.com>
2018-11-08 15:22:59 +0100

    Update js/refactor.js
    Refactor Arrow Function Expressions

--- js/refactor.js
+++ js/refactor.js
@@ -1,6 +1,4 @@
-const multiplyByTen = (num) => {
-	return 10 * num;
-}
+const multiplyByTen = num => 10 * num;
 
 const printToTen = () => {
 	for (let i = 1; i <= 10; i++) {
@@ -8,6 +6,4 @@ const printToTen = () => {
 	}
 }
 
-const addUs = (num1, num2) => {
-	console.log(num1 + num2);
-}
+const addUs = (num1, num2) => console.log(num1 + num2);
